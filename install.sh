eabde6a3799de8ee069173ff60b7ae01143b1663
hien.le <hien.le@ericsson.com>
2019-08-03 00:42:30 -0400

    add zsh config

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 CWD=$(pwd)
 
-echo -e "\u001b[33;1mBacking up old files...\u001b[0m"
+echo "Backing up old files..."
 mv -iv ~/.zshrc ~/.zshrc.old
 mv -iv ~/.zsh ~/.zsh.old
 mv -iv ~/.vimrc ~/.vimrc.old
 mv -iv ~/.vim ~/.vim.old
 
-echo -e "\u001b[36;1mAdding symlinks...\u001b[0m"
+echo "Adding symlinks..."
 ln -sfnv $CWD/.zshrc ~/.zshrc
 ln -sfnv $CWD/.zsh ~/.zsh
 ln -sfnv $CWD/.vimrc ~/.vimrc
 ln -sfnv $CWD/.vim ~/.vim
 
-echo -e "\u001b[32;1mDone.\u001b[0m"
+echo "Done."
 
