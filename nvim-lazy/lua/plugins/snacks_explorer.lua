68515fe31af4b64f6d4507ddf7df3a05f50e8e28
Hien Le <hien.le@privacy-analytics.com>
2025-03-23 15:31:41 -0400

    update

---
+++ nvim-lazy/lua/plugins/snacks_explorer.lua
@@ -0,0 +1,18 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+          ignored = true,
+          layout = {
+            layout = {
+              width = 50,
+            },
+          },
+        },
+      },
+    },
+  },
+}
